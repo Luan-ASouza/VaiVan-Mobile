a6afe3ae2908b614d70e7a0194dc832a3c49cd9d
Laura Uberti <55255035+LauraUberti@users.noreply.github.com>
2026-05-17 16:07:07 -0300

    ajustes finais - Laura

--- app/src/main/java/com/example/trabalhograua/AdicionarPassageiroActivity.java
+++ app/src/main/java/com/example/trabalhograua/AdicionarPassageiroActivity.java
@@ -1,5 +1,6 @@
 package com.example.trabalhograua;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,12 +35,15 @@ public class AdicionarPassageiroActivity extends AppCompatActivity {
                 String nascimento = edtNascimento.getText().toString().trim();
 
                 if (!nome.isEmpty() && !nascimento.isEmpty()) {
-                    // ADICIONA NA LISTA GLOBAL: Guarda na memória temporária do app
+                    // 1. Salva temporariamente na memória RAM do aplicativo
                     DadosGlobais.listaPassageiros.add(nome);
 
-                    Toast.makeText(AdicionarPassageiroActivity.this,
-                            "Passageiro " + nome + " cadastrado!", Toast.LENGTH_SHORT).show();
-                    finish(); // Volta para a tela anterior
+                    // 2. Redireciona imediatamente para a tela de procurar motorista/rota
+                    Intent intent = new Intent(AdicionarPassageiroActivity.this, ProcurarMotoristaActivity.class);
+                    startActivity(intent);
+
+                    // 3. Fecha a tela de cadastro por baixo
+                    finish();
                 } else {
                     Toast.makeText(AdicionarPassageiroActivity.this,
                             "Por favor, preencha o Nome e a Data de Nascimento!", Toast.LENGTH_SHORT).show();
