5317d52ca5a19a62f2d9f248372314bd57f59b3a
Schleder <gameplayrs9@gmail.com>
2026-07-07 00:07:05 -0300

    Adição da api dos maps (de novo) e do gpx

--- app/src/main/java/com/example/trabalhograua/MotoristaActivity.kt
+++ app/src/main/java/com/example/trabalhograua/MotoristaActivity.kt
@@ -1,5 +1,9 @@
 package com.example.trabalhograua
 
+import android.graphics.Color
+import android.util.Xml
+import com.google.android.gms.maps.model.PolylineOptions
+import org.xmlpull.v1.XmlPullParser
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -30,27 +34,69 @@ class MotoristaActivity : BaseActivity(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
 
-        val btnCancelar = findViewById<Button>(R.id.btnCancelar)
-
-        btnCancelar.setOnClickListener {
-            finish()
-        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
 
         mMap = googleMap
 
-        val local = LatLng(-30.0346, -51.2177)
+        desenharRota()
+    }
 
-        mMap.addMarker(
-            MarkerOptions()
-                .position(local)
-                .title("Estudantes próximos")
-        )
+    private fun desenharRota() {
 
-        mMap.moveCamera(
-            CameraUpdateFactory.newLatLngZoom(local, 13f)
-        )
+        val parser = Xml.newPullParser()
+
+        parser.setInput(resources.openRawResource(R.raw.rota), "UTF-8")
+
+        val pontos = mutableListOf<LatLng>()
+
+        var event = parser.eventType
+
+        while (event != XmlPullParser.END_DOCUMENT) {
+
+            if (event == XmlPullParser.START_TAG &&
+                parser.name == "trkpt") {
+
+                val lat = parser.getAttributeValue(null, "lat").toDouble()
+                val lon = parser.getAttributeValue(null, "lon").toDouble()
+
+                pontos.add(
+                    LatLng(lat, lon)
+                )
+            }
+
+            event = parser.next()
+        }
+
+        if (pontos.isNotEmpty()) {
+
+            mMap.addPolyline(
+                PolylineOptions()
+                    .addAll(pontos)
+                    .width(12f)
+                    .color(Color.parseColor("#F6B12F"))
+                    .geodesic(true)
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(pontos.first())
+                    .title("Início da rota")
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(pontos.last())
+                    .title("Destino")
+            )
+
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pontos.first(),
+                    14f
+                )
+            )
+        }
     }
 }
